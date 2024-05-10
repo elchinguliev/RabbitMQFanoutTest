d08a4d767a2c3076d39c5b10bd9ab644d94e8ff3
Quliy_av90 <Quliy_av90@STHQ0118-07>
2024-05-10 10:03:44 +0400

    Add project files.

---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitConsumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri=new Uri("amqps://wkrhegxa:6UytImCBsgpRvzEKMSy4Ji-T68l7kOxl@roedeer.rmq.cloudamqp.com/wkrhegxa");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue:queueName,exchange:"logs",routingKey:string.Empty); 
+
+            var consumer=new EventingBasicConsumer(channel);
+            channel.BasicConsume(queueName,true, consumer);
+            consumer.Received+=Consumer_Received;
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            var message=Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine($"Recieved message : {message}");
+        }
+    }
+}
---
+++ RabbitFanoutTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitPublisher
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri=new Uri("amqps://wkrhegxa:6UytImCBsgpRvzEKMSy4Ji-T68l7kOxl@roedeer.rmq.cloudamqp.com/wkrhegxa"); 
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+            channel.QueueDeclare("",true,false,false);
+            while (true)
+            {
+                Console.ReadLine();
+                var message = "Hello RabbitMQ Fanout";
+                var body=Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("logs", "", null, body);
+                Console.WriteLine("Message sent successfully");
+            }
+        }
+    }
+}
